6147834cb6b1372e8a94e89895e3ec384d9bfe30
Arko <arkoroyb@gmail.com>
2023-07-29 11:52:56 +0600

    pcbuilder page sidebar new logic implemented

--- src/pages/pc-builder/index.js
+++ src/pages/pc-builder/index.js
@@ -2,70 +2,115 @@ import RootLayout from "@/components/Layouts/RootLayout";
 import { Divider, Menu } from "antd";
 import Link from "next/link";
 import { useSelector } from "react-redux";
-import styles from '../../styles/pc-builder.module.css'
+import styles from "../../styles/pc-builder.module.css";
 
-function getItem(label, key, icon, children, type, to) {
-  return {
-    key,
-    icon,
-    children,
-    label: type === "link" ? <Link href={to}>{label}</Link> : label,
-    type,
-  };
+function isCategoryIncluded(products, categoryToCheck) {
+  return products.find((product) => product.category === categoryToCheck);
 }
-
-const categoryItems = [
-  getItem("Categories", "sub1", null, [
-    getItem("CPU / Processor", "1", null, null, "link", "/pc-builder/cpu"),
-    getItem("Motherboard", "2", null, null, "link", "/pc-builder/motherboard"),
-    getItem("RAM", "3", null, null, "link", "/pc-builder/ram"),
-    getItem(
-      "Power Supply Unit",
-      "4",
-      null,
-      null,
-      "link",
-      "/pc-builder/power-supply"
-    ),
-    getItem(
-      "Storage Device",
-      "5",
-      null,
-      null,
-      "link",
-      "/pc-builder/storage-device"
-    ),
-    getItem("Monitor", "6", null, null, "link", "/pc-builder/monitor"),
-  ]),
-];
 
 const PCBuilder = () => {
   const { pcbuilder: pcdata } = useSelector((state) => state);
-  console.log("pcdata",pcdata);
+
+  console.log("pcdata", pcdata);
+  const products = pcdata.products || [];
   return (
     <div className="pageHeight content-body">
       <div
         style={{
           display: "flex",
-          // justifyContent: "space-between"
         }}
       >
         <div className={`${styles["pcBuilderNav"]}`}>
-          {/* <Menu
-            style={{
-              width: 256,
-            }}
-            defaultSelectedKeys={["sub1"]}
-            defaultOpenKeys={["sub1"]}
-            mode="inline"
-            items={categoryItems}
-          /> */}
-          <Link href="/pc-builder/cpu">CPU/Processor</Link>
-          <Link href="/pc-builder/motherboard">MOTHERBOARD</Link>
-          <Link href="/pc-builder/ram">RAM</Link>
-          <Link href="/pc-builder/power-supply">POWER SUPPLY UNIT</Link>
-          <Link href="/pc-builder/storage-device">STORAGE DEVICE</Link>
-          <Link href="/pc-builder/MONITOR">MONITOR</Link>
+          <Link href="/pc-builder/cpu" className={isCategoryIncluded(products, "CPU") ? 'disabled' : ''}>
+            CPU/Processor{" "}
+            {isCategoryIncluded(products, "CPU") && (
+              <span
+                style={{
+                  color: "green",
+                  padding: "0 5px",
+                  background: "orange",
+                  borderRadius: "5px",
+                }}
+              >
+                selected
+              </span>
+            )}{" "}
+          </Link>
+          <Link className={isCategoryIncluded(products, "CPU") ? 'disabled' : ''} href="/pc-builder/motherboard" >
+            MOTHERBOARD{" "}
+            {isCategoryIncluded(products, "MOTHERBOARD") && (
+              <span
+                style={{
+                  color: "green",
+                  padding: "0 5px",
+                  background: "orange",
+                  borderRadius: "5px",
+                }}
+              >
+                selected
+              </span>
+            )}
+          </Link>
+          <Link className={isCategoryIncluded(products, "RAM") ? 'disabled' : ''} href="/pc-builder/ram">
+            RAM{" "}
+            {isCategoryIncluded(products, "RAM") && (
+              <span
+                style={{
+                  color: "green",
+                  padding: "0 5px",
+                  background: "orange",
+                  borderRadius: "5px",
+                }}
+              >
+                selected
+              </span>
+            )}
+          </Link>
+          <Link className={isCategoryIncluded(products, "POWER SUPPLY") ? 'disabled' : ''} href="/pc-builder/power-supply">
+            POWER SUPPLY UNIT{" "}
+            {isCategoryIncluded(products, "POWER SUPPLY") && (
+              <span
+                style={{
+                  color: "green",
+                  padding: "0 5px",
+                  background: "orange",
+                  borderRadius: "5px",
+                }}
+              >
+                selected
+              </span>
+            )}
+          </Link>
+          <Link className={isCategoryIncluded(products, "STORAGE DEVICE") ? 'disabled' : ''} href="/pc-builder/storage-device">
+            STORAGE DEVICE{" "}
+            {isCategoryIncluded(products, "STORAGE DEVICE") && (
+              <span
+                style={{
+                  color: "green",
+                  padding: "0 5px",
+                  background: "orange",
+                  borderRadius: "5px",
+                }}
+              >
+                selected
+              </span>
+            )}
+          </Link>
+          <Link className={isCategoryIncluded(products, "MONITOR") ? 'disabled' : ''} href="/pc-builder/MONITOR">
+            MONITOR{" "}
+            {isCategoryIncluded(products, "MONITOR") && (
+              <span
+                style={{
+                  color: "green",
+                  padding: "0 5px",
+                  background: "orange",
+                  borderRadius: "5px",
+                }}
+              >
+                selected
+              </span>
+            )}
+          </Link>
         </div>
         <div style={{ marginLeft: "50px", marginTop: "40px", width: "600px" }}>
           <h4 className="">Your PC Parts</h4>
